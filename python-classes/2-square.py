61834dff1cedcd704f10e228b0f00ac9569b5200
ahmadabdullayew <ahmadabdullayevcontact@gmail.com>
2025-12-11 13:30:56 +0000

    Area of a square

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,5 +1,8 @@
-#!/usr/bin/python3
+k#!/usr/bin/python3
 class Square:
+    """
+    A class that defines a square by its size.
+    """
     def __init__(self, size=0):
         try:    
             if not isinstance(size, (int, float)):
