54475c14bb6a8c0495d5f9605f47656123da87f9
ahmadabdullayew <ahmadabdullayevcontact@gmail.com>
2025-12-11 09:48:34 +0000

    Square with size

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
+"""
+0-square.py - Bu modül, Square sınıfını tanımlar.
+Bu, ALX Python OOP görevlerinin 0. bölümü için zorunludur.
+"""
+
 class Square:
+    """
+    Bir kareyi, özel (private) bir 'size' niteliği ile tanımlayan sınıf.
+    """
     def __init__(self, size):
+        """
+        Yeni bir Square örneğini başlatır.
+        
+        Args:
+            size (int): Karenin boyutu (kenar uzunluğu).
+        """
         self.__size = size
